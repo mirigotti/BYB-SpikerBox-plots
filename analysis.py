3e285000ee24686b2349851da7afcbea482bb8ac
Mattia Thompson <mattiathompson@BMESNEL-MBP14-B.local>
2025-01-26 19:47:18 -0500

    add initial files

---
+++ analysis.py
@@ -0,0 +1,160 @@
+# %%
+# imports
+
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+# class to load data and perform analyses 
+
+class SpikeAnalysis:
+
+    def __init__(self):
+
+        self.data = None
+        self.events = None
+        self.neuron_events = None
+
+    def load_events(self, events_file):
+
+        events = [] 
+        neuron_events = []  
+
+        with open(events_file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                parts = line.split(',')
+                if len(parts) != 2:
+                    continue
+                name, timestamp = parts
+                name = name.strip()
+                try:
+                    timestamp = float(timestamp.strip())
+                except ValueError:
+                    print(f"Invalid timestamp: {timestamp} in line: {line}")
+                    continue
+                if name.startswith('_ch'):
+                    neuron_events.append((name, timestamp))
+                elif not name.startswith('_'):
+                    events.append((name, timestamp))
+
+        self.events = events
+        self.neuron_events = neuron_events
+
+        # change units from s to ms
+        self.events = [(name, timestamp * 1000) for name, timestamp in self.events]
+        self.neuron_events = [(name, timestamp * 1000) for name, timestamp in self.neuron_events]
+
+
+        print(f"Loaded {len(events)} experiment events.")
+        print(f"Loaded {len(neuron_events)} neurons events.")
+
+        # neuron_ids = np.unique([n for n, t in self.neuron_events])
+        # neuron_id = neuron_ids[0]
+
+        # spike_times = [t for n, t in self.neuron_events if n == neuron_id]
+
+        # spike_times = {}
+        # for neuron, timestamp in self.neuron_events:
+        #     if neuron not in spike_times:
+        #         spike_times[neuron] = []
+        #     spike_times[neuron].append(timestamp)
+
+        # self.spike_times = spike_times
+
+    # base_path = '/Users/mattiathompson/Documents/Projects/spike-analysis/samples'
+    # # file_name = 'BYB_Recording_2018-02-28_14.01.34.wav'
+    # # file_name = 'BYB_Recording_2018-02-28_14.11.33.wav'
+    # file_name = 'BYB_Recording_2018-09-20_13.12.48.wav'
+    # data_file_path = os.path.join(base_path, file_name)
+    # events_file_path = data_file_path.replace('.wav', '-events.txt')
+
+    # if os.path.exists(events_file_path):
+    #     events, neuron_events = load_events(events_file_path)
+    #     print(f"Loaded {len(events)} experiment events.")
+    #     print(f"Loaded {len(neuron_events)} neurons events.")
+    # else:
+    #     print("No events file found.")
+ 
+    def trialize_data(self, pre_event_duration=-1000, post_event_duration=5000):
+
+        self.pre_event_duration = pre_event_duration
+        self.post_event_duration = post_event_duration
+
+        trials = []
+        for event, timestamp in self.events:
+            trial_start = timestamp + self.pre_event_duration
+            trial_end = timestamp + self.post_event_duration
+            trial_spikes_aligned = [(t - timestamp) for n, t in self.neuron_events if t >= trial_start and t <= trial_end]
+            trials.append((event, trial_spikes_aligned))
+        
+        self.trials = trials
+
+        trials_matrix = np.zeros((len(trials), int((self.post_event_duration - self.pre_event_duration)/10)))
+        for i, (event, trial_spikes_aligned) in enumerate(trials):
+            for spike_time in trial_spikes_aligned:
+                spike_bin = int((spike_time - self.pre_event_duration)/10)
+                trials_matrix[i, spike_bin] += 1
+
+        self.trials_matrix = trials_matrix
+
+
+    def get_trial_data(self):
+
+        return self.trials, self.trials_matrix
+
+    def set_bin_size(self, bin_size):
+
+        self.bin_size = bin_size
+
+    def get_psth_data(self):
+        
+        binned_trials = []
+        
+        for event, spike_times in self.trials:
+            bins = np.arange(self.pre_event_duration, self.post_event_duration+self.bin_size, self.bin_size)
+            binned_spikes, _ = np.histogram(spike_times, bins=bins)
+            binned_trials.append((event, binned_spikes))
+        
+        t_binned = np.arange(self.pre_event_duration, 
+                             self.post_event_duration, 
+                             self.bin_size)
+
+        binned_trials_arr = np.stack([binned_spikes for event, binned_spikes in binned_trials])
+        psth_data = np.mean(binned_trials_arr, axis=0) / (self.bin_size / 1000)
+
+        return t_binned, psth_data
+
+# %%
+# trialize data
+
+# pre_event_duration = -5
+# post_event_duration = 10
+# bin_size = 0.1
+
+# def trialize_data(events, neuron_events, pre_event_duration=-1, post_event_duration=5):
+#     trials = []
+#     for event, timestamp in events:
+#         trial_start = timestamp + pre_event_duration
+#         trial_end = timestamp + post_event_duration
+#         trial_spikes_aligned = [t - timestamp for n, t in neuron_events if t >= trial_start and t <= trial_end]
+#         trials.append((event, trial_spikes_aligned))
+#     return trials
+
+# trials = trialize_data(events, neuron_events, pre_event_duration=pre_event_duration, post_event_duration=post_event_duration)
+
+
+
+# binned_trials = bin_spikes(trials, bin_size=bin_size, 
+#                            pre_event_duration=pre_event_duration, post_event_duration=post_event_duration)
+
+
+# plt.axvline(0, color='w', linestyle='-', linewidth=1)
+# plt.show()
+
+# psth = np.mean(binned_trials_arr, axis=0) / bin_size
+
---
+++ gui.py
@@ -0,0 +1,304 @@
+import sys
+from PyQt6.QtWidgets import (QApplication, QMainWindow,
+                            QVBoxLayout, QHBoxLayout, 
+                            QWidget, QPushButton, 
+                            QFileDialog, QLineEdit, 
+                            QLabel, QSpinBox, 
+                            QDialog, QMessageBox,
+                            QStackedLayout
+                            )  
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+from analysis import SpikeAnalysis # import the SpikeAnalysis class from analysis.py
+import os
+from PyQt6.QtGui import QPixmap, QFont
+
+class RasterCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=6, height=3, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi, constrained_layout=True)
+        self.ax = fig.add_subplot(111)#, position=[0.1, 0.15, 0.85, 0.8])
+        self.ax.axis('off')
+        super().__init__(fig)
+
+    def update_plot(self, analysis):
+
+        trial_data, trials_matrix = analysis.get_trial_data()
+
+        self.ax.clear()
+
+        self.ax.imshow(1-trials_matrix, 
+                       aspect='auto', 
+                       cmap='gray', 
+                       interpolation='none',
+                    #    vmax=1, vmin=0,
+                       extent=[analysis.pre_event_duration, analysis.post_event_duration, 0, len(trial_data)])
+
+        # for tr, trial in enumerate(trial_data):
+        #     event, spike_times = trial
+        #     self.ax.plot(spike_times, tr + np.ones_like(spike_times), 'o', markersize=2)
+        
+        self.ax.axvline(0, color='k', linestyle='--', linewidth=1)
+
+        self.ax.set_xlim([analysis.pre_event_duration, analysis.post_event_duration])
+
+        self.ax.set_xlabel('Time (ms)')
+        self.ax.set_ylabel('Trial #')
+
+        self.draw()
+
+class PSTHCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=6, height=3, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi, constrained_layout=True)
+        self.ax = fig.add_subplot(111)#, position=[0.1, 0.15, 0.85, 0.8])
+        self.ax.axis('off')
+        super().__init__(fig)
+
+    def update_plot(self, analysis):
+
+        t_binned, psth_data = analysis.get_psth_data()
+
+        self.ax.clear()
+
+        self.ax.plot(t_binned, psth_data)
+        self.ax.axvline(0, color='k', linestyle='--', linewidth=1)
+
+        self.ax.set_xlim([t_binned[0], t_binned[-1]])
+        self.ax.set_ylim(bottom=0)
+
+        self.ax.set_xlabel('Time (ms)')
+        self.ax.set_ylabel('Firing rate (Hz)')
+        
+        self.draw()
+
+
+# class InteractiveCanvas(MplCanvas):
+
+#     def __init__(self, parent=None, width=5, height=4, dpi=100):
+#         super().__init__(parent, width, height, dpi)
+#         self.points, = self.axes.plot([0.5, 0.75], [0.5, 0.75], 'ro', picker=5)
+#         self.selected_point = None
+#         self.cid = self.mpl_connect('pick_event', self.on_pick)
+#         self.cid_motion = self.mpl_connect('motion_notify_event', self.on_motion)
+#         self.cid_release = self.mpl_connect('button_release_event', self.on_release)
+
+#     def on_pick(self, event):
+#         if event.artist != self.points:
+#             return
+#         self.selected_point = event.ind[0]
+
+#     def on_motion(self, event):
+#         if self.selected_point is None:
+#             return
+#         x, y = event.xdata, event.ydata
+#         if x is None or y is None:
+#             return
+#         self.points.set_data(np.insert(self.points.get_xdata(), self.selected_point, x), 
+#                              np.insert(self.points.get_ydata(), self.selected_point, y))
+#         self.draw()
+
+#     def on_release(self, event):
+#         self.selected_point = None
+
+# class InteractivePlotDialog(QDialog):
+
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.setWindowTitle("Interactive Plot")
+#         self.canvas = InteractiveCanvas(self, width=5, height=4, dpi=100)
+#         layout = QVBoxLayout()
+#         layout.addWidget(self.canvas)
+#         self.setLayout(layout)
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Spike Analysis")
+        self.setFixedSize(1200, 720)
+
+        # Load wav button and text field
+        self.wav_button = QPushButton('Load .wav File')
+        self.wav_button.clicked.connect(self.load_wav_file)
+        self.wav_button.setFont(QFont("Arial",16))
+        self.wav_file_label = QLineEdit()
+        self.wav_file_label.setReadOnly(True)
+        self.wav_file_label.setFont(QFont("Arial",14))
+
+        # Load events button and text field
+        self.txt_button = QPushButton('Load .txt File')
+        self.txt_button.clicked.connect(self.load_txt_file)
+        self.txt_button.setFont(QFont("Arial",16))
+        self.txt_file_label = QLineEdit()
+        self.txt_file_label.setReadOnly(True)
+        self.txt_file_label.setFont(QFont("Arial",14))
+
+        # Time input fields
+        self.window_start_label = QLabel('Window start (ms):')
+        self.window_start_label.setFont(QFont("Arial",16))
+        self.window_start_input = QLineEdit()
+        self.window_start_input.setFont(QFont("Arial",14))
+        self.window_end_label = QLabel('Window end (ms):')
+        self.window_end_label.setFont(QFont("Arial",16))
+        self.window_end_input = QLineEdit()
+        self.window_end_input.setFont(QFont("Arial",14))
+        self.bin_size_label = QLabel('Bin size (ms):')
+        self.bin_size_label.setFont(QFont("Arial",16))
+        self.bin_size_input = QSpinBox()
+        self.bin_size_input.setFont(QFont("Arial",14))
+        input_layout = QVBoxLayout()
+        input_layout.addWidget(self.window_start_label)
+        input_layout.addWidget(self.window_start_input)
+        input_layout.addWidget(self.window_end_label)
+        input_layout.addWidget(self.window_end_input)
+        input_layout.addWidget(self.bin_size_label)
+        input_layout.addWidget(self.bin_size_input)
+        # Set default values
+        self.window_start_input.setText('-500')
+        self.window_end_input.setText('2500')
+        self.bin_size_input.setValue(50)
+
+        self.generate_button = QPushButton('Generate plots')
+        self.generate_button.setStyleSheet("background-color: green")
+        self.generate_button.clicked.connect(self.generate_plots)
+        self.generate_button.setFont(QFont("Arial",16))
+
+        self.export_label = QLabel('Export plots:')
+        self.export_label.setFont(QFont("Arial",16))
+        self.export_input = QLineEdit()
+        self.export_input.setFont(QFont("Arial",14))
+        self.export_input.setText('output')
+        self.export_button = QPushButton('Export')
+        self.export_button.clicked.connect(self.export_plots)
+        self.export_button.setFont(QFont("Arial",16))
+
+        # Right side plots
+        self.canvas1 = RasterCanvas(self)
+        self.canvas2 = PSTHCanvas(self)
+
+        # Layouts
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.wav_button)
+        left_layout.addWidget(self.wav_file_label)
+        left_layout.addStretch(1)
+
+        left_layout.addWidget(self.txt_button)
+        left_layout.addWidget(self.txt_file_label)
+        left_layout.addStretch(1)
+
+        left_layout.addLayout(input_layout)
+        left_layout.addWidget(self.generate_button)
+        left_layout.addStretch(1)
+
+        left_layout.addWidget(self.export_label)
+        left_layout.addWidget(self.export_input)
+        left_layout.addWidget(self.export_button)
+
+        plot_layout = QVBoxLayout()
+        plot_layout.addWidget(self.canvas1)
+        plot_layout.addWidget(self.canvas2)
+
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(left_layout)
+        main_layout.addLayout(plot_layout)
+
+        # Create a central widget and set the layout
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+
+        self.setCentralWidget(central_widget)
+
+        self.analysis = SpikeAnalysis()
+
+    def load_wav_file(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open .wav File", "", "WAV Files (*.wav);;All Files (*)")
+        # check if file is a wav file
+        # if file_name and not file_name.endswith('.wav'):
+        if file_name:
+            self.wav_file_label.setText(file_name)
+
+    def load_txt_file(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open .txt File", "", "Text Files (*.txt);;All Files (*)")
+        if file_name:
+            self.txt_file_label.setText(file_name)
+        
+        self.analysis.load_events(file_name)
+
+    def generate_plots(self):
+        # check if self.window_start.text() is a number
+        # if not self.window_start_input.text().isnumeric():
+        #     # error popup window
+        #     self.show_error_popup('Window start must be a number')
+        #     return
+        # # check if self.window_end.text() is a number
+        # if not self.window_end_input.text().isnumeric():
+        #     self.show_error_popup('Window end must be a number')
+        #     return
+        # check if self.integer_input.value() is an integer
+        if self.bin_size_input.value() <= 0:
+            self.show_error_popup('Bin size must be a positive integer')
+            return
+        
+        self.analysis.trialize_data(pre_event_duration=int(self.window_start_input.text()), 
+                                    post_event_duration=int(self.window_end_input.text()))
+        self.analysis.set_bin_size(self.bin_size_input.value())
+
+        self.canvas1.update_plot(self.analysis)
+        self.canvas2.update_plot(self.analysis)
+
+    def set_save_directory(self):
+
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if directory:
+            self.save_directory_path = directory
+
+        # save plots to directory
+
+
+    def export_plots(self):
+        
+        self.set_save_directory()
+        
+        if not hasattr(self, 'save_directory_path') or not self.save_directory_path:
+            self.show_error_popup('Please select a directory to save the plots.')
+            return
+
+        file_name = self.export_input.text()
+
+        canvas1_path = os.path.join(self.save_directory_path, f'{file_name}_raster_plot.png')
+        canvas2_path = os.path.join(self.save_directory_path, f'{file_name}_psth.png')
+
+        self.canvas1.figure.savefig(canvas1_path)
+        self.canvas2.figure.savefig(canvas2_path)
+
+        self.show_info_popup(f'Plots saved to {self.save_directory_path}')
+
+    # def show_interactive_plot(self):
+    #     dialog = InteractivePlotDialog(self)
+    #     dialog.exec()
+
+    def show_error_popup(self, message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Icon.Critical)
+        msg.setText(message)
+        msg.setWindowTitle("Error")
+        msg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msg.exec()
+
+    def show_info_popup(self, message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.setText(message)
+        msg.setWindowTitle("Information")
+        msg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msg.exec()
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
